10f9d095e2587b4914751350ab68730fd8dc26d4
Nina Kin <matikin9@gmail.com>
2024-03-13 22:50:01 -0700

    reposition legend for desktop

--- js/index.js
+++ js/index.js
@@ -1,10 +1,33 @@
-document.getElementById('legend-close').addEventListener('click', () => {
-    document.getElementById('legend').style.display = 'none';
-    document.getElementById('legend-mini').style.display = 'flex';
+let showMini = false;
 
+document.getElementById('legend-close').addEventListener('click', () => {
+    showMini = true;
+    adjustMiniDisplay();
 });
 
 document.getElementById('legend-mini').addEventListener('click', () => {
-    document.getElementById('legend').style.display = 'block';
-    document.getElementById('legend-mini').style.display = 'none';
+    showMini = false;
+    adjustMiniDisplay();
+});
+
+function adjustMiniDisplay() {
+    if (showMini) {
+        document.getElementById('legend').style.display = 'none';
+        document.getElementById('legend-mini').style.display = 'flex';
+    } else {
+        document.getElementById('legend').style.display = 'block';
+        document.getElementById('legend-mini').style.display = 'none';
+    }
+}
+
+window.addEventListener('resize', () => {
+    var w = document.documentElement.clientWidth;
+    var h = document.documentElement.clientHeight;
+
+    if (w > 600) {
+        document.getElementById('legend').style.display = 'block';
+        document.getElementById('legend-mini').style.display = 'none';
+    } else {
+        adjustMiniDisplay();
+    }
 });
